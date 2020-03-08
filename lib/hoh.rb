b9262375d98dede2ee67b7bb52c2a961f17e5a35
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 20:04:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,10 +55,7 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
   hoh = {
-      {
-        :label => PORTION_1[:label],
-      :sub_category => PORTION_1[:sub_category]
-    }
+      {:label => PORTION_1[:label], :sub_category => PORTION_1[:sub_category]}
   }
 pp hoh
   puts hoh
